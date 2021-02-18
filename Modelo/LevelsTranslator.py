706c0447e3453714a3b1c62226674b1868f43ec6
Guicebr <guillermo.cebrian.serrano@gmail.com>
2021-02-18 13:11:46 +0100

    Añadidos los metodos para calcular las experiencias y niveles según la experiencia que recibe y las experiencias de cada nivel

--- Modelo/LevelsTranslator.py
+++ Modelo/LevelsTranslator.py
@@ -13,6 +13,10 @@ FILE = "XP_Level.xml"
 # logger = logging.getLogger(__name__)
 
 import xml.etree.cElementTree as ET
+
+
+
+
 class LevelsTranslator:
     """Clase encargaada de almacenar"""
 
@@ -48,4 +52,45 @@ class LevelsTranslator:
             rootdict[int(temp_dict["level"])-1] = {"exptoup": int(temp_dict["exptoup"]), "totalxp": int(temp_dict["totalxp"])}
         return rootdict
 
+    def getLV_EXP (self, exp):
+        """Metodo que recibe una cantidad de experiencia de tipo int, y devuelve una lista de pares clave valor
+        con los niveles posibles y la experiencia que se tendría en cada uno de ellos [Nivel, Experiencia]
+        IN: 79825637
+        OUT: [[44,26325637],[45,200...],...]"""
 
+        LV40EXP = int(self.level_dict[40-1]['totalxp'])
+        if exp < LV40EXP:
+            lv = self.getLevel_XP(exp)
+            return [[lv, self.getExpLV(exp, lv)]]
+        else:
+            lv_exp = []
+            levels = self.getLevels_XP(exp)
+            for leveli in levels:
+                lv_exp.append([leveli, self.getExpLV(exp, leveli)])
+            return lv_exp
+        return None
+
+
+    def getExpLV(self, exp, lv):
+        return exp - self.level_dict[lv - 1]['totalxp']
+
+    def getLevel_XP(self, exp):
+        """Encuentra el nivel de un jugador dada la experiencia.
+        IN: Int Experiencia
+        OUT: Int Nivel"""
+
+        for leveli in range(len(self.level_dict)):
+            if(exp - self.level_dict[leveli]['totalxp']) < 0:
+                return leveli
+
+    def getLevels_XP(self, exp):
+        """Encuentra el nivel de un jugador dada la experiencia.
+                IN: Int Experiencia
+                OUT: List[Int] Nivel"""
+        lvposibles = []
+        for leveli in range(40-1, len(self.level_dict)):
+            print(leveli)
+            if (exp - self.level_dict[leveli]['totalxp']) > 0:
+                lvposibles.append(leveli+1)
+        print(lvposibles)
+        return lvposibles
