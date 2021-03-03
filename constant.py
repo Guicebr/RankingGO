b38a09fb091c569ad4237e943437c4611c34df2b
Guicebr <guillermo.cebrian.serrano@gmail.com>
2021-03-03 12:30:44 +0100

    context.user_data[STRINGS]

---
+++ constant.py
@@ -0,0 +1,7 @@
+CONTEXT_VAR_USERDBID = "userdbid"
+CONTEXT_VAR_USERDBNICK = "userdbnick"
+CONTEXT_VAR_TRTYPE = "tr_type"
+CONTEXT_VAR_AMOUNT = "amount"
+CONTEXT_VAR_TMPNICK = "nick" # Nick introducido por el usuario, que no esta validado aún
+CONTEXT_VAR_OCRUSER_VALID = "ocr_user_valid"
+CONTEXT_VAR_OCRUSER = "ocr_user"
