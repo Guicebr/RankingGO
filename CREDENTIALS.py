2f071f6cfda74a82b7a286431b73bc8d41947802
Guicebr <guillermo.cebrian.serrano@gmail.com>
2021-03-24 11:45:49 +0100

    Añadida Telegram API

--- CREDENTIALS.py
+++ CREDENTIALS.py
@@ -10,6 +10,7 @@ BOT_TOKEN = "1161816070:AAH03yJ7nyZs5r-M5CtZV2XMiF78YXDYsjk"
 #                 'unix_socket': '/var/run/mysqld/mysqld.sock',  # mysql unix socket file
 #             })
 
+# https://api.telegram.org/1161816070:AAH03yJ7nyZs5r-M5CtZV2XMiF78YXDYsjk/getupdates
 conf = dict(telegram_token="1161816070:AAH03yJ7nyZs5r-M5CtZV2XMiF78YXDYsjk",
             mysql={
                 'user': 'wicitelebot',  # mysql user
@@ -18,4 +19,7 @@ conf = dict(telegram_token="1161816070:AAH03yJ7nyZs5r-M5CtZV2XMiF78YXDYsjk",
                 'db': 'RankingPOGO',  # mysql database
                 'port': 3306,  # mysql port
                 'unix_socket': '/var/run/mysqld/mysqld.sock',  # mysql unix socket file
-            })
+            },
+            telegram_api=3818151,
+            telegram_hash="52a698395ad88ac99841a5b7a8a0ccf3",
+            phone="+34610931373")
