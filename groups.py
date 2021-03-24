0dad80ece9be65fdc04174e75455c1b7e5e9f13f
Guicebr <guillermo.cebrian.serrano@gmail.com>
2021-03-24 11:44:34 +0100

    Acabar Comando GroupInfo

--- groups.py
+++ groups.py
@@ -1,6 +1,6 @@
 import logging
 import constant as CONS
-from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, InlineKeyboardButton, InlineKeyboardMarkup
+
 from telegram import Update
 from telegram.ext import Updater, CallbackContext
 from Database.dbhelper import DBHelper
@@ -9,9 +9,9 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
                     level=logging.INFO, filename='example.log')
 logger = logging.getLogger(__name__)
 
+
 def authgroups(update: Update, context: CallbackContext):
-    """Verificar si un grupo esta en la BD y si no se añade yse devuelve el groupid"""
-    # TODO: Verificar si un grupo esta en la BD y si no se añade y se devuelve el groupid
+    """Verificar si un grupo esta en la BD y si no se añade y se devuelve el groupid"""
 
     group_tgid = update.message.chat_id
     group_name = update.message.chat.title
@@ -41,6 +41,7 @@ def authgroups(update: Update, context: CallbackContext):
             if dbconn:
                 dbconn.close()
 
+
 def add_user_groups(group_id, user_tgid) -> None:
     """Add user to a telegroup in db"""
     try:
@@ -62,6 +63,21 @@ def groups_new_chat_members_handler(update: Update, context: CallbackContext) ->
     new_users = update.message.new_chat_members
     group_name = update.message.chat.title
 
+    print([group_id, group_name, new_users])
+
+    # Insertar usuario en el grupo asociado a la BD
+    for new_user in new_users:
+        logger.info("New user in group %s %s %s", group_name, new_user.name, new_user.id)
+        add_user_groups(group_id, new_user.id)
+
+def groups_talk_chat_member_handler(update: Update, context: CallbackContext) -> None:
+
+    # Obtener telegroup DBID
+    group_id = authgroups(update, context)
+    new_users = [update.message.from_user]
+    group_name = update.message.chat.title
+
+    print([group_id, group_name, new_users])
     # Insertar usuario en el grupo asociado a la BD
     for new_user in new_users:
         logger.info("New user in group %s %s %s", group_name, new_user.name, new_user.id)
@@ -84,25 +100,45 @@ def groups_left_chat_member_handler(update: Update, context: CallbackContext) ->
         if dbconn:
             dbconn.close()
 
-def group_info(update:Update, context: CallbackContext) -> None:
-    # TODO: Añadir metodo a command handler. Solo admins
-    group_id = authgroups(update, context)
+
+def group_info(update: Update, context: CallbackContext) -> None:
+    """"""
+    # TODO: Comment
+    # Comando group_info, solo admins del grupo
+
     group_tgid = update.message.chat_id
+    user_id = update.message.from_user.id
+    user = context.bot.get_chat_member(group_tgid, user_id)
+    if user.status not in ['creator', "administrator"]:
+        return None
+
+    group_id = authgroups(update, context)
     group_name = update.message.chat.title
-    user_id = update.message.from_user
     chat_users = int(context.bot.get_chat_members_count(group_tgid))
+
     try:
         dbconn = DBHelper()
         usersdb = int(dbconn.group_users_count(group_id))
         usersvalidated = int(dbconn.group_usersvalidated_count(group_id))
-        print(group_name, usersdb)
-        print(group_name, usersvalidated)
-        # TODO: Preparar mensaje. Reponder en privado
+
+        logger.info("%s, %s -> %s", user.user.name, group_name, "group_info")
+
+        # Preparar mensaje. Reponder en privado
+        txt = ("GRUPO: %s\n"
+               "Usuarios en el grupo: %d\n"
+               "Almacenados en la BD: %d\n"
+               "Validados con el bot: %d") % (group_name, chat_users, usersdb, usersvalidated)
+        context.bot.sendMessage(chat_id=user_id, text=txt)
+
+        # Borra el mensaje del comando. Se necesita permisos de administrador
+        context.bot.deleteMessage(group_tgid, update.message.message_id)
 
     except Exception as e:
         logger.debug(e)
     finally:
         if dbconn:
             dbconn.close()
+
+
 
 
