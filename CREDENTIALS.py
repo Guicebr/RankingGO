be6258708d72c84284bea575672456815e93d39d
Guillermo Cebrián Serrano <guillermo.cebrian.serrano@gmail.com>
2023-05-04 12:29:29 +0200

    Update CREDENTIALS.py

--- CREDENTIALS.py
+++ CREDENTIALS.py
@@ -2,9 +2,9 @@ VISION_KEY_PATH = ''
 BOT_TOKEN = "1161816070:AAH03yJ7nyZs5r-M5CtZV2XMiF78YXDYsjk"
 # conf = dict(telegram_token="1161816070:AAH03yJ7nyZs5r-M5CtZV2XMiF78YXDYsjk",
 #             mysql={
-#                 'user': 'wicitelebot',  # mysql user
-#                 'passwd': 'wicipass',  # mysql password
-#                 'host': 'localhost',  # mysql host
+#                 'user': '',  # mysql user
+#                 'passwd': '',  # mysql password
+#                 'host': '',  # mysql host
 #                 'db': 'RankingPOGO',  # mysql database
 #                 'port': 3306,  # mysql port
 #                 'unix_socket': '/var/run/mysqld/mysqld.sock',  # mysql unix socket file
@@ -13,9 +13,9 @@ BOT_TOKEN = "1161816070:AAH03yJ7nyZs5r-M5CtZV2XMiF78YXDYsjk"
 # https://api.telegram.org/1161816070:AAH03yJ7nyZs5r-M5CtZV2XMiF78YXDYsjk/getupdates
 conf = dict(telegram_token="1161816070:AAH03yJ7nyZs5r-M5CtZV2XMiF78YXDYsjk",
             mysql={
-                'user': 'wicitelebot',  # mysql user
-                'passwd': 'wicipass',  # mysql password
-                'host': '192.168.1.30',  # mysql host
+                'user': '',  # mysql user
+                'passwd': '',  # mysql password
+                'host': '',  # mysql host
                 'db': 'RankingPOGO',  # mysql database
                 'port': 3306,  # mysql port
                 'unix_socket': '/var/run/mysqld/mysqld.sock',  # mysql unix socket file
